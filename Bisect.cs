2e6a132e9ea42625d9c500e836c17784389dd28e
Luis Saenz <Luis.A.Saenz@outlook.com>
2020-03-13 20:54:42 -0400

    This is a sample program that guess the user input from 1-10 using the bisection method.

---
+++ Bisect.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessMyNumber
+{
+    public class Bisect
+    {
+        public static ConsoleKeyInfo cki = Console.ReadKey(true);
+        internal List<int> bisect1(List<int> list, int x)
+        {
+            list.Clear();
+            if (x == 5)
+            {
+                Console.WriteLine("You're answer is 5. LOSER");
+                Environment.Exit(0);
+            }
+
+            else if (x != 5)
+            {
+                Console.Clear();
+                Console.Write("Is your value higher or lower than 5? (h / l) ");
+                if (cki.Key == ConsoleKey.H)
+                {
+                    if (x! < 5)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You lied to me..");
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        int j = 0;
+                        for (int i = 6; i <= 10; i++)
+                        {
+                            list.Insert(j, i);
+                            j++;
+                        }
+                    }
+                }
+                else if (cki.Key == ConsoleKey.L)
+                {
+                    if (x! > 5)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You lied to me..");
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        for (int i = 1; i <= 4; i++)
+                        {
+                            list.Insert(i, i);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("You did not choose an option provided");
+                }
+            }
+            return list;
+        }
+
+
+        internal List<int> bisect2(List<int> list, int user)
+        {
+            if (user == 3)
+            {
+                Console.Clear();
+                Console.WriteLine("You're answer is 3. LOSER");
+                Environment.Exit(0);
+            }
+            else if (user != 3)
+            {
+                Console.Clear();
+                Console.Write("Is your value higher or lower than 3? (h / l) ");
+                ConsoleKeyInfo cki = Console.ReadKey();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.H:
+                        if (user! < 3)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You lied to me..");
+                            Environment.Exit(0);
+                        }
+                        else
+                        {
+                            Console.WriteLine("You're answer is 4.");
+                            Environment.Exit(0);
+                        }
+                        break;
+                    case ConsoleKey.L:
+                        if (user! > 3)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You lied to me..");
+                            Console.WriteLine("You're answer is 4.");
+                            Environment.Exit(0);
+                        }
+                        else
+                        {
+                            list.Clear();
+                            int j = 0;
+                            for (int i = 1; i <= 2; i++)
+                            {
+                                list.Insert(j, i);
+                                j++;
+                            }
+                        }
+                        break;
+                }
+            }
+            return list;
+        }
+
+        internal List<int> bisect3(List<int> list, int user)
+        {
+            if (user == 8)
+            {
+                Console.Clear();
+                Console.WriteLine("You're answer is 8. LOSER");
+                Environment.Exit(0);
+            }
+            else if (8 != user)
+            {
+                Console.Clear();
+                Console.Write("Is your value higher or lower than 8? (h / l) ");
+                ConsoleKeyInfo cki = Console.ReadKey();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.H:
+                        if (user! < 8)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You lied to me..");
+                            Environment.Exit(0);
+                        }
+                        else
+                        {
+                            list.Clear();
+
+                            int j = 0;
+                            for (int i = 9; i <= 10; i++)
+                            {
+                                list.Insert(j, i);
+                                j++;
+                            }
+                        }
+                        break;
+                    case ConsoleKey.L:
+                        if (user! > 8)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You lied to me..");
+                            Environment.Exit(0);
+                        }
+                        else
+                        {
+                            list.Clear();
+
+                            int j = 0;
+                            for (int i = 6; i <= 7; i++)
+                            {
+                                list.Insert(j, i);
+                                j++;
+                            }
+                        }
+                        break;
+                }
+            }
+            return list;
+        }
+        internal List<int> bisect4(List<int> list3, int user)
+        {
+            if (user == 2)
+            {
+                Console.Clear();
+                Console.WriteLine("You're answer is 2. LOSER");
+                Environment.Exit(0);
+            }
+            else if (user != 2)
+            {
+                Console.Clear();
+                Console.WriteLine("You're answer is 1. LOSER");
+                Environment.Exit(0);
+            }
+            return list3;
+        }
+        internal List<int> bisect5(List<int> list3, int user, int x, int y)
+        {
+            if (x == user)
+            {
+                Console.Clear();
+                Console.WriteLine($"You're answer is {x}. LOSER");
+                Environment.Exit(0);
+            }
+            else if (x != user)
+            {
+                Console.Clear();
+                Console.WriteLine($"You're answer is {y}. LOSER");
+                Environment.Exit(0);
+            }
+            return list3;
+        }
+    }
+}
---
+++ Run.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuessMyNumber
+{
+    public class Run
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            bool valid = false;
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Bisect bisect = new Bisect();
+            try
+            {
+                do
+                {
+                    Console.Write("Select a number between 1-10: ");
+
+
+                    var x = Console.ReadLine();
+                    Console.Clear();
+                    valid = int.TryParse(x, out a);
+
+                } while (!valid);
+
+                if (a > 10 || a < 1)
+                {
+                    throw new Exception("You did not input a number between 1 - 10.");
+                }
+
+                List<int> list2 = bisect.bisect1(list, a);
+
+                if (list2.Contains(8))
+                {
+                    List<int> list3 = bisect.bisect3(list2, a);
+                    if (list3.Contains(6))
+                    {
+                        List<int> list4 = bisect.bisect5(list3, a, 6, 7);
+                    }
+                    else if (list3.Contains(9))
+                    {
+                        List<int> list4 = bisect.bisect5(list3, a, 9, 10);
+                    }
+                }
+
+                else if (list2.Contains(3))
+                {
+                    List<int> list3 = bisect.bisect2(list2, a);
+                    if (list3.Contains(2))
+                    {
+                        List<int> list4 = bisect.bisect4(list3, a);
+                    }
+                }
+
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
