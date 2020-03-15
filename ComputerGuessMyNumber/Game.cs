210107168c0f3eb82957f1b5ea372e2dac9f12ee
Luis Saenz <Luis.A.Saenz@outlook.com>
2020-03-14 23:24:48 -0400

    User guesses computer's number. With little hints.

---
+++ ComputerGuessMyNumber/Game.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputerGuessMyNumber
+{
+    public class Game
+    {
+        public static void Run(Dictionary <int, int> dict, int u)
+        {
+            if (u == Program.comp)
+            {
+                Console.WriteLine("\nYou guessed the number.");
+                Environment.Exit(0);
+            }
+            else if (u > Program.comp)
+            {
+                Console.WriteLine("\nYour guess was too high.");
+                dict.Clear();
+               
+
+                for (int i = 1; i <= u - 1; i++)
+                {
+                    dict.Add(i, i);
+                }
+
+                HighOrLOw(dict, u - 1);
+            }
+            else if (u < Program.comp)
+            {
+                Console.WriteLine("\nYour guess was too low.");
+                dict.Clear();
+                
+
+                for (int i = u + 1; i <= 1000; i++)
+                {
+                    dict.Add(i, i);
+                }
+
+                HighOrLOw(dict, u + 1);
+            }
+        }
+        private static void HighOrLOw(Dictionary<int, int> dict, int o, int f = 1)
+        {
+            int u = Program.UserInput(dict);
+
+            if (u == Program.comp)
+            {
+                Console.WriteLine("\nYou guessed the number.");
+                Environment.Exit(0);
+            }
+            else if (u > Program.comp)
+            {
+                Console.WriteLine("\nYour guess was too high.");
+                if (u > o)
+                {
+                    var minK = dict.Keys.Min();
+                    dict.Clear();
+                    for (int i = minK; i <= u - 1; i++)
+                    {
+                        dict.Add(i, i);
+                    }
+                }
+                else
+                {
+                    var minK = dict.Keys.Min();
+
+                    dict.Clear();
+
+                    for (int i = minK; i <= u - 1; i++)
+                    {
+                        dict.Add(i, i);
+                    }
+                }
+
+                HighOrLOw(dict, u - 1);
+            }
+            else if (u < Program.comp)
+            {
+                Console.WriteLine("\nYour guess was too low.");
+
+                if (u > o)
+                {
+                    var maxK = dict.Keys.Max();
+                    dict.Clear();
+                    for (int i = u + 1; i <= maxK; i ++)
+                    {
+                        dict.Add(i, i);
+                    }
+                }
+                else if (u == o)
+                {
+                    Console.WriteLine($"The only possible answer left is {dict.Keys.Max()}");
+                }
+                else
+                {
+                    dict.Clear();
+                    for (int i = u + 1; i <= o; i++)
+                    {
+                        dict.Add(i, i);
+                    }
+                }
+
+                HighOrLOw(dict, u + 1);
+            }
+        }
+    }
+}
---
+++ ComputerGuessMyNumber/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComputerGuessMyNumber
+{
+    public class Program
+    {
+            public static Random r = new Random();
+            public static int comp = r.Next(1, 1000);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GUESS MY NUMBER!!!!!!!!!!");
+            Console.WriteLine("\nHint 1, it is between 1 and 1000.");
+
+            Dictionary<int, int> dict = new Dictionary<int, int> { };
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                dict.Add(i, i);
+            }
+
+
+            try
+            {
+                int input = UserInput(dict);
+                Game.Run(dict, input);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+
+
+
+        public static int UserInput(Dictionary<int, int> dict)
+        {
+            bool valid = false;
+
+            int a;
+            do
+            {
+                Console.Write("\nInput Guess: ");
+
+                var x = Console.ReadLine();
+                Console.Clear();
+                valid = int.TryParse(x, out a);
+
+            } while (!valid);
+
+            if (!dict.ContainsValue(a))
+            {
+                throw new Exception("Input is not in range");
+            }
+
+            return a;
+        }
+    }
+}
+
